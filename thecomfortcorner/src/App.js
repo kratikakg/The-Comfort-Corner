ddd3e1de0d67bb61d755478ba13ce4800d0886a2
kratikagupta0306 <132834070+kratikakg@users.noreply.github.com>
2023-10-25 20:47:25 +0530

    debug react issues

---
+++ thecomfortcorner/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;

d7fd0fcf92e47f8bf7de8407a3ab869d8c6651b5
kratikagupta0306 <132834070+kratikakg@users.noreply.github.com>
2023-10-25 21:16:57 +0530

    update tailwind.config.js

--- thecomfortcorner/src/App.js
+++ thecomfortcorner/src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
+import "./App.css";
+
 const App = () => {
   return (
     <div>
--- thecomfortcorner/src/components/Navbar.jsx
+++ thecomfortcorner/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full flex py-6 justify-between items-center">
+      <img src=".\assets\logo.png" alt="logo " className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 ">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;

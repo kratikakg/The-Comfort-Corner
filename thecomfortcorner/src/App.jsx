46c651c0510a4fcd010f17abebff92cc2bfef9ae
kratikagupta0306 <132834070+kratikakg@users.noreply.github.com>
2023-11-05 11:04:14 +0530

    Update Hero

--- thecomfortcorner/src/App.jsx
+++ thecomfortcorner/src/App.jsx
@@ -1,24 +1,14 @@
 import styles from "./style";
 
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 const App = () => (
   <div className="w-full max-w-[1440px] mx-auto bg-white overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
     <div className={`${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
-      </div>
-    </div>
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Footer />
+        <Features />
       </div>
     </div>
   </div>
--- thecomfortcorner/src/components/Hero.jsx
+++ thecomfortcorner/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { hero } from "../data";
 
+import Stats from "./Stats";
 const Hero = () => {
   //destructure hero
   const { title, subtitle, buttonText } = hero;
@@ -17,6 +18,9 @@ const Hero = () => {
         <button className="bg-[rgba(255,255,255,0.4)] hover:bg-[rgba(255,255,255,0.5)] px-[35px] py-[9px] mb-[160px] text-xl rounded-md backdrop-blur-md transition lg:px-[80px] lg:py-[16px] lg:mb-[194px]">
           {buttonText}
         </button>
+        <div>
+          <Stats />
+        </div>
       </div>
     </section>
   );
